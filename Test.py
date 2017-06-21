03440a07c591c8fedfa9327682fa26684e1f8697
PixelInks <carmenle520@gmail.com>
2017-06-20 23:19:10 -0700

    Day 2: Learned about functions

--- Test.py
+++ Test.py
@@ -1,34 +1,38 @@
-#My first program
+#Functions are defined here
+def add(firstNumber, secondNumber):
+    x = firstNumber+secondNumber
+    return x
 
+def sub(firstNumber, secondNumber):
+    y = firstNumber-secondNumber
+    return y
+
+def multi(firstNumber, secondNumber): 
+    z = firstNumber*secondNumber
+    return z
+
+def div(firstNumber, secondNumber): 
+    c = firstNumber/secondNumber
+    return c
+
+#Program starts here
 mode = input("Welcome to the calculator. Enter 1 for addition and 2 for subtraction 3 for multiplication, 4 for division. ")
 mode = int(mode)
-if mode == 1: 
-    x = input("Please type in your numbers.")
-    y = input("Please type in your other number.")
-    x = float(x)
-    y = float(y)
-    z = x+y
-    print(z)
-elif mode ==2:
-    x = input("please type in  your number ")
-    y = input("pleae type in your other number ")
-    x = float(x)
-    y = float(y)
-    z = x-y
-    print(z)
-elif mode ==3:
-    x = input("please type in  your number ")
-    y = input("pleae type in your other number ")
-    x = float(x)
-    y = float(y)
-    z = x*y
-    print(z)
-elif mode ==4:
-    x = input("please type in  your number ")
-    y = input("pleae type in your other number ")
-    x = float(x)
-    y = float(y)
-    z = x/y
-    print(z)
+
+a = float(input("please enter your first number "))
+b = float(input("please enter your second number "))
+
+if mode == 1:
+    output = add(a,b)
+    print (output)
+elif mode == 2:
+    output = sub(a,b)
+    print (output)
+elif mode == 3:
+    output = multi(a,b)
+    print (output)
+elif mode == 4:
+    output = div(a,b)
+    print (output)
 else:
-    print ("Not a valid input. Bye!")
+    print ("Have a nice day")
