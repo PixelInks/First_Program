5d9e95d36174057216c49821a01dc7acdefad9f4
PixelInks <carmenle520@gmail.com>
2017-06-24 21:06:01 -0700

    Learned how to use While loop

--- Test.py
+++ Test.py
@@ -16,23 +16,27 @@ def div(firstNumber, secondNumber):
     return c
 
 #Program starts here
-mode = input("Welcome to the calculator. Enter 1 for addition and 2 for subtraction 3 for multiplication, 4 for division. ")
-mode = int(mode)
+n="r"
+while n=="R" or n=="r":
+  
+    mode = input("Welcome to the calculator. Enter 1 for addition and 2 for subtraction 3 for multiplication, 4 for division. ")
+    
+    mode = int(mode)
 
-a = float(input("please enter your first number "))
-b = float(input("please enter your second number "))
+    a = float(input("please enter your first number "))
+    b = float(input("please enter your second number "))
 
-if mode == 1:
-    output = add(a,b)
-    print (output)
-elif mode == 2:
-    output = sub(a,b)
-    print (output)
-elif mode == 3:
-    output = multi(a,b)
-    print (output)
-elif mode == 4:
-    output = div(a,b)
-    print (output)
-else:
-    print ("Have a nice day")
+    if mode == 1:
+        output = add(a,b)
+        print (output)
+    elif mode == 2:
+        output = sub(a,b)
+        print (output)
+    elif mode == 3:
+        output = multi(a,b)
+        print (output)
+    elif mode == 4:
+        output = div(a,b)
+        print (output)
+    
+    n = input("Would you like to continue? Type 'R' to continue use. ")
